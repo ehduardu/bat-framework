ee942867a3023477618c6b09affe5f50a1b39170
dudsjrsilva <dudsjrsilva@gmail.com>
2022-09-22 22:34:00 -0300

    fix: add check to pass the create folder error

--- postBuild.js
+++ postBuild.js
@@ -5,7 +5,12 @@ const copyRecursiveSync = (src, dest) => {
   const stats = fs.statSync(src);
   const isDirectory = stats.isDirectory();
   if (isDirectory) {
-    fs.mkdirSync(dest);
+    try {
+      fs.mkdirSync(dest);
+    } catch (error) {
+      if (!error.message.includes('EEXIST: file already exists')) throw error;
+    }
+
     fs.readdirSync(src).forEach(function(childItemName) {
       copyRecursiveSync(path.join(src, childItemName), path.join(dest, childItemName));
     });
