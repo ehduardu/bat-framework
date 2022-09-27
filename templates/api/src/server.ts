02ca36daaf91a1d5e9b597005819543fe27fa1fb
dudsjrsilva <dudsjrsilva@gmail.com>
2022-09-26 23:06:05 -0300

    feat: update api template

--- templates/api/src/server.ts
+++ templates/api/src/server.ts
@@ -1,4 +1,13 @@
-function doGet(e: GoogleAppsScript.Events.AppsScriptHttpRequestEvent) {
-  const { name } = e.parameter;
+function doGet(e: GoogleAppsScript.Events.DoGet) {
+  const { name } = e.parameter; //GET Parameter
   return ContentService.createTextOutput(`Hello ${name}`);
+}
+
+interface IBody{
+  name: string;
+}
+
+function doPost(e: GoogleAppsScript.Events.DoPost) {
+  const body = JSON.parse(e.postData.contents) as IBody //POST Body
+  return ContentService.createTextOutput(`Hello ${body.name}`);
 }
